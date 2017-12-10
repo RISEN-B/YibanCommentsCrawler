1efb47bbb477f9ace1d9dd13a1f62f526a7f6c5a
87314 <873142511@qq.com>
2017-12-10 20:28:56 +0800

    The first update.

--- Start.py
+++ Start.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import requests
 
 
 def get_comments():
-    print()
+    url = "https://q.yiban.cn/app/index/appid/135991"
+    r = requests.get(url)
+    print(r.text)
 
 
 if __name__ == '__main__':
